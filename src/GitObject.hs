7deb26f78d3a71001e47ac2edb560ddd0549bf2a
James Oh <jo9347@cs.rit.edu>
2023-11-09 17:29:06 -0500

    Change gitobject types

--- src/GitObject.hs
+++ src/GitObject.hs
@@ -9,31 +9,37 @@ module GitObject
     GitObjectHash,
     newBlob,
     newTree,
+    newCommit,
   )
 where
 
 import Data.ByteString (ByteString)
 import Data.Time.Clock (UTCTime)
 
--- GitBlob = (file content in binary, filename)
-type GitBlob = (ByteString, String)
+-- GitBlob = (byteSize, file content in binary, filename)
+type GitBlob = (Int, ByteString, String)
 
-newBlob :: ByteString -> String -> GitObject
-newBlob content filename = Blob (content, filename)
+newBlob :: Int -> ByteString -> String -> GitObject
+newBlob byteSize content filename = Blob (byteSize, content, filename)
 
--- GitTree = [(permission bits, name, sha1 hash)]
-type GitTree = [(String, String, ByteString)]
+-- GitTree = (byteSize, [(permission bits, name, sha1 hash)])
+type GitTree = (Int, [(String, String, ByteString)])
 
-newTree :: GitTree -> GitObject
-newTree = Tree
+newTree :: Int -> [(String, String, ByteString)] -> GitObject
+newTree byteSize elems = Tree (byteSize, elems)
 
--- GitCommit = (tree, parent, author, committer, message, timestamp)
-newtype GitCommit = GitCommit (GitTree, Maybe [ByteString], String, String, String, UTCTime)
+-- GitCommit = (tree hash, parent hashes, author, committer, message)
+newtype GitCommit = GitCommit (ByteString, [ByteString], String, String, String, UTCTime)
+  deriving (Show)
 
 data GitObject = Tree GitTree | Commit GitCommit | Blob GitBlob
 
+newCommit :: ByteString -> [ByteString] -> String -> String -> String -> UTCTime -> GitObject
+newCommit tree parents author commiter message timestamp = Commit (GitCommit (tree, parents, author, commiter, message, timestamp))
+
 type GitObjectHash = (GitObject, ByteString)
 
+-- TODO: delete
 -- newTree :: GitTree -> GitObject
 -- newTree tree = Tree tree
 
@@ -50,6 +56,9 @@ instance Show GitObject where
   show :: GitObject -> String
   show (Tree tree) = "Tree " ++ show tree
   show (Blob blob) = "Blob " ++ show blob
-  show (Commit (GitCommit (gt, parents, author, commiter, message, timestamp))) = "Commit " ++ "(" ++ commitStr ++ ")"
-    where
-      commitStr = show gt ++ ", " ++ show parents ++ ", " ++ author ++ ", " ++ commiter ++ ", " ++ message ++ ", " ++ show timestamp
+  show (Commit commit) = "Commit " ++ show commit
+
+-- TODO: delete
+-- show (Commit (GitCommit (gt, parents, author, commiter, message, timestamp))) = "Commit " ++ "(" ++ commitStr ++ ")"
+--   where
+--     commitStr = show gt ++ ", " ++ show parents ++ ", " ++ author ++ ", " ++ commiter ++ ", " ++ message ++ ", " ++ show timestamp
