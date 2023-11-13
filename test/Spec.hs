357dfc8fe24db4dae9230791400d2c3e15c4a645
Chen Guo <cg2531@rit.edu>
2023-11-12 22:51:00 -0500

    test: update package configuration and init Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,19 @@
+import qualified Data.ByteString.Lazy.Char8 as BSLC
+import HaskGit
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: IO TestTree
+tests =
+  testGroup "HaskGit Tests"
+    <$> sequence
+      [ testCase "testHash" testHashTest
+      ]
+
+-- FIXME: as `testHash` is an IO function, should it be tested?
+-- or, pure function is better? and how to implement it?
+testHashTest :: IO TestTree
+testHashTest = undefined
