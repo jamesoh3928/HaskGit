64e2891bc06a76f88689bd87f2fd121133a29d23
Jack Oh <jo8842@g.rit.edu>
2023-11-17 17:41:12 -0500

    Working on adding cmd args for update ref

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,52 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+
 module Main (main) where
 
-import HaskGit
-
-import System.Console.CmdArgs
 import qualified Data.ByteString.Char8 as B
 import Experiment
+import HaskGit
+import System.Console.CmdArgs
 
 data HaskGit
   = Show {hash :: String}
   | WriteTree {filename :: FilePath}
   | HashObject {filename :: FilePath}
+  | UpdateRef {refdest :: String, refsrc :: String}
   deriving (Data, Typeable, Show, Eq)
 
 -- NOTE: Functionality is not implemented yet
 writeTreeMode :: HaskGit
-writeTreeMode = WriteTree {filename = def &= typFile &= argPos 0} &=
-  help "haskgit writeTree <filename>\nCreate a tree object from the current index"
+writeTreeMode =
+  WriteTree {filename = def &= typFile &= argPos 0}
+    &= help "haskgit writeTree <filename>\nCreate a tree object from the current index"
 
 showMode :: HaskGit
-showMode = Show {hash = def &= argPos 0}&=
-  help "haskgit show <object>\nShows various types of git objects" 
+showMode =
+  Show {hash = def &= argPos 0}
+    &= help "haskgit show <object>\nShows various types of git objects"
 
 hashObjectMode :: HaskGit
-hashObjectMode = HashObject {filename = def &= typFile &= argPos 0} &=
-  help "haskgit hashobject <filename>\nCompute object ID" 
+hashObjectMode =
+  HashObject {filename = def &= typFile &= argPos 0}
+    &= help "haskgit hashobject <filename>\nCompute object ID"
+
+updateRefMode :: HaskGit
+updateRefMode =
+  UpdateRef
+    { refdest = def &= argPos 0,
+      refsrc = def &= argPos 1
+    }
+    &= auto
+    &= help "haskgit updateref <ref_dest> <ref_src> \n Update the ref."
 
 cmdModes :: HaskGit
-cmdModes = modes [showMode, hashObjectMode]
+cmdModes = modes [showMode, hashObjectMode, updateRefMode]
 
 runCommand :: HaskGit -> IO ()
 runCommand cmd = case cmd of
   Show hash -> gitShow (B.pack hash)
   HashObject filename -> testHash filename
+  UpdateRef refdest refsrc -> gitUpdateRef refdest refsrc
 
 main :: IO ()
 main = do
